f39a1b15fdb6dae6ca1dbf6f70c783c006fb5bc2
John Mitsch <jomitsch@redhat.com>
2020-05-25 11:37:00 -0400

    Working on bg jobs

--- pasta.go
+++ pasta.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"io"
 	"os"
@@ -11,6 +12,12 @@ import (
 	"strings"
 	"syscall"
 )
+
+type ShellCommand struct {
+	command    string
+	args       []string
+	background bool
+}
 
 func runBuiltIns(command string, args []string) bool {
 	switch command {
@@ -35,45 +42,100 @@ func runBuiltIns(command string, args []string) bool {
 	}
 }
 
+func handleInputErr(err error) {
+	if err != nil {
+		// Handle Ctrl+D to stop program.
+		if err == io.EOF {
+			fmt.Println("\nArrivederci!")
+			os.Exit(1)
+		}
+		fmt.Println("Mama Mia! Something went wrong!")
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
+func buildCommand(input string, background bool) ShellCommand {
+	command := ShellCommand{}
+	input = strings.Trim(input, " \n")
+	parsedInput := strings.Split(input, " ")
+	command.command = parsedInput[0]
+	command.args = parsedInput[1:]
+	command.background = background
+	return command
+}
+
+func buildAllCommands(input string) []ShellCommand {
+	allCommands := make([]ShellCommand, 0)
+	// matching all " & " but ideally should change since one could pass in a quoted " & " as
+	// part of a command that would be ignored by a typical shell
+	if strings.HasSuffix(input, "&") || strings.Contains(input, " & ") {
+		commands := strings.Split(input, "&")
+		lastJobIndex := len(commands) - 1
+		lastJob := commands[lastJobIndex]
+		backgroundJobs := commands[:lastJobIndex]
+		foregroundJobs := strings.Split(lastJob, "&&")
+		for _, bg := range backgroundJobs {
+			allCommands = append(allCommands, buildCommand(bg, true))
+		}
+		for _, fg := range foregroundJobs {
+			allCommands = append(allCommands, buildCommand(fg, false))
+		}
+	} else {
+		userCommands := strings.Split(input, "&&")
+		for _, cmd := range userCommands {
+			allCommands = append(allCommands, buildCommand(cmd, false))
+		}
+	}
+	return allCommands
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		fmt.Print("🍝 ")
 		input, err := reader.ReadString('\n')
-		input = strings.TrimRight(input, "\n")
-		if err != nil {
-			// Handle Ctrl+D to stop program.
-			if err == io.EOF {
-				fmt.Println("\nArrivederci!")
-				os.Exit(1)
+		handleInputErr(err)
+		for _, command := range buildAllCommands(input) {
+			if runBuiltIns(command.command, command.args) {
+				continue
 			}
-			fmt.Println("Mama Mia! Something went wrong!")
-			fmt.Fprintln(os.Stderr, err)
-		}
 
-		parsedInput := strings.Split(input, " ")
-		userCmd := parsedInput[0]
-		args := parsedInput[1:]
+			cmd := exec.Command(command.command, command.args...)
+			cmd.Env = append(os.Environ())
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
 
-		if runBuiltIns(userCmd, args) {
-			continue
-		}
+			fmt.Print("👨‍🍳 ")
+			startErr := cmd.Start()
+			if startErr != nil {
+				fmt.Printf("Something went wrong!\n %s\n", startErr)
+			}
 
-		c := make(chan os.Signal)
-		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-		go func() {
-			<-c
-			os.Exit(1)
-		}()
-		cmd := exec.Command(userCmd, args...)
-		cmd.Env = append(os.Environ())
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
+			ctx := context.Background()
 
-		fmt.Print("👨‍🍳 ")
-		if err := cmd.Run(); err != nil {
-			fmt.Printf("Something went wrong!\n %s\n", err)
+			// trap Ctrl+C and call cancel on the context
+			ctx, cancel := context.WithCancel(ctx)
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, os.Interrupt)
+			defer func() {
+				signal.Stop(c)
+				cancel()
+			}()
+			go func() {
+				select {
+				case <-c:
+					cancel()
+				case <-ctx.Done():
+				}
+			}()
+
+			waitErr := cmd.Wait()
+
+			if waitErr != nil {
+				fmt.Println("")
+				break
+			}
 		}
 	}
 }
